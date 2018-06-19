a29d5459bc210d1599175761f54bd8b0f159a7e1
wilperi <32159152+wilperi@users.noreply.github.com>
2018-06-19 21:19:54 +0300

    ville.190618.1
    vuosikulutukset

--- js/script.js
+++ js/script.js
@@ -374,8 +374,11 @@ var vm = new Vue({
 
 						for (var h = 0; h <= 11; h++) {
 							this.monthlyHeatConsumption[h].value += parseFloat(response.data[months[h]]);
+							this.yearlyHeatConsumption += response.data[months[h]];
 						}
 	
+						this.kWhvalue.push(response.data);  //adds the received object to kWhvalue
+
 					},
 					function (error) {
 						// handle error
@@ -387,6 +390,7 @@ var vm = new Vue({
 
 						for (var e = 0; e <= 11; e++) {
 							this.monthlyElecConsumption[e].value += parseFloat(response.data[months[e]]);
+							this.yearlyElecConsumption += response.data[months[e]];
 						}
 						
 					},
@@ -400,21 +404,6 @@ var vm = new Vue({
 
 		},
 		drawResultChart: function (heat, elec) {
-			// var yearlyHeat = 0;
-			// var yearlyElec = 0;
-
-			// for (var i = 0; i < heat.length; i++) {
-			// 	console.log(heat[i].value);
-			// 	yearlyHeat += heat[i];
-			// }
-
-			// for (var i = 0; i < elec.length; i++) {
-			// 	console.log(elec[i].value);
-			// 	yearlyElec += elec[i];
-			// }
-
-			// this.yearlyHeatConsumption = yearlyHeat;
-			// this.yearlyElecConsumption = yearlyElec;
 
 			google.charts.load('current', { 'packages': ['corechart'] });
 			google.charts.setOnLoadCallback(drawChart);
@@ -422,7 +411,7 @@ var vm = new Vue({
 			function drawChart() {
 				
 				var data = google.visualization.arrayToDataTable([
-					['Kuukausi', 'Lämmitysenergian kulutus', 'Laitesähkön kulutus'],
+					['Kuukausi', 'Lämmitys', 'Sähkö'],
 					['Tammi', heat[0].value, elec[0].value],
 					['Helmi', heat[1].value, elec[1].value],
 					['Maalis', heat[2].value, elec[2].value],
@@ -440,11 +429,13 @@ var vm = new Vue({
 				var options = {
 					backgroundColor: '#f5f5f5',
 					title: 'kWh / kk',
-					legend: { position: 'bottom' },
+					legend: { position: 'right' },
 					bars: 'vertical',
 					vAxis: { format: '' },
 					colors: ['#C52F03', '#328FB2'],
-					backgroundColor: '#fff'
+					backgroundColor: '#fff',
+					width: '691',
+					height: '360'
 				};
 
 				var chart = new google.visualization.ColumnChart(document.getElementById('resultchart'));
